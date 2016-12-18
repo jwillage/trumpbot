660ccef81864cca49318d5bfd35fa40db524603a
jwillage <jwillage@gmail.com>
2016-12-18 11:36:06 -0500

    read from archive

--- start.R
+++ start.R
@@ -3,25 +3,37 @@ library(dplyr)
 library(tidytext)
 library(ggplot2)
 library(quanteda)
+library(jsonlite)
 
-
-setup_twitter_oauth(consumer_key, consumer_secret, access_token, token_secret)
+creds <- readLines("creds.txt")
+setup_twitter_oauth(creds[1], creds[2], creds[3], creds[4])
 
 all_tweets <- userTimeline("realdonaldtrump", n = 3200, includeRts = F)
-source <- sapply(tweets, function(y) sub("</a>", "", sub("<(.*?)>", "", y$statusSource)))
-# there was a post by @drob a couple months ago on how all trumps pesonal tweets are from iphone
-# tweets from android were from his campaign team and not in the classic trump voice
-iphone <- which(source == "Twitter for iPhone")
-all_texts <- sapply(tweets, function(x) x$text)
-texts <- all_texts[iphone]
-tweets <- data.frame(text = texts, line = 1:length(iphone), stringsAsFactors = FALSE)
+
+updateTweets <- function(all_tweets){
+  recent <- userTimeline("realdonaldtrump", n = 3200, includeRts = F, sinceID = all_tweets[[1]]$id)
+  all_tweets <- c(all_tweets, recent)
+  all_tweets
+}
+
+getArchive <- function(years) {
+  dfs <- lapply(years, function(x) 
+          fromJSON(paste0("http://www.trumptwitterarchive.com/data/", x, ".json")))
+  bind_rows(dfs)
+}
+
+archived <- getArchive(2010:2016)
+# http://varianceexplained.org/r/trump-tweets/ suggests iphone == trump, android == staff
+# tweets from other sources appear to be in the style of trump
+archived <- archived[archived$source != "Twitter for Android", ]
+texts <- archived$text
+tweets <- data.frame(text = texts, line = 1:length(texts), stringsAsFactors = FALSE)
 
 tidy_tweets <- tweets %>% unnest_tokens(word, text)
 #tidy_tweets <- tidy_tweets %>% anti_join(stop_words)
-
 tidy_tweets %>% count(word, sort=T) %>%
   anti_join(stop_words) %>%
-  filter(n > 15) %>% mutate(word=reorder(word, n)) %>%
+  filter(n > 200) %>% mutate(word=reorder(word, n)) %>%
   ggplot(aes(word, n)) +
   geom_bar(stat="identity") +
   coord_flip()
@@ -30,15 +42,13 @@ sentiments <- tidy_tweets %>% inner_join(get_sentiments("nrc")) %>% count(word, 
   
 sentiments %>% 
   filter(n > 1, sentiment %in% c("positive", "negative")) %>%
-mutate(n = ifelse(sentiment=="negative", -n, n)) %>%
+  mutate(n = ifelse(sentiment=="negative", -n, n)) %>%
   mutate(word = reorder(word, n)) %>%
-ggplot(aes(word, n, fill=sentiment)) +
+  ggplot(aes(word, n, fill=sentiment)) +
   geom_bar(stat="identity") +
   coord_flip()
 
-utf8 <- stringi::stri_enc_isutf8(texts)
-texts <- texts[utf8]
-tweets <- data.frame(text = texts, line = 1:length(texts), stringsAsFactors = FALSE)
+# break down for NLP
 tweet_sentences <- tweets %>% unnest_tokens(sent, text, token="sentences")
 tweet_sentences$sent <- paste("zzstart", tweet_sentences$sent, "zzend")
 uni_tweets <- tweet_sentences %>% unnest_tokens(word, sent, to_lower = FALSE)
@@ -61,16 +71,12 @@ gen_tweet()
 
 gen_tweet <- function() {
   tweet <- list(start="zzstart")
-  
-  # start a random sentence
-  # tweet <- c(tweet,
-  #            bigrams[bigrams$idx == "zzstart", ]
-  #            [round(runif(1, 1, nrow(bigrams[bigrams$idx == "zzstart", ])), 0), "gram"])
   loop = TRUE
   while(loop) {
+    l <- tweet[[length(tweet)]]
     tweet <- c(tweet,
-               bigrams[bigrams$idx == tweet[[length(tweet)]], ]
-               [round(runif(1, 1, nrow(bigrams[bigrams$idx == tweet[[length(tweet)]], ])), 0), "gram"])    
+               bigrams[bigrams$idx == l, ]
+               [round(runif(1, 1, nrow(bigrams[bigrams$idx == l, ])), 0), "gram"])    
     if (tweet[[length(tweet)]] == "zzend")
       loop = FALSE
     
